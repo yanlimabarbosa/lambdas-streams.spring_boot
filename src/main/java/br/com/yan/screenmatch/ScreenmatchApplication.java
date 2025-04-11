12c831bfeafff79de10f1e5624b42739bb93b37a
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-11 19:27:15 -0300

    feat: modeling data from api using JACKSON

--- src/main/java/br/com/yan/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/yan/screenmatch/ScreenmatchApplication.java
@@ -1,6 +1,8 @@
 package br.com.yan.screenmatch;
 
+import br.com.yan.screenmatch.model.DadosSerie;
 import br.com.yan.screenmatch.service.ConsumoApi;
+import br.com.yan.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,8 +18,11 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		ConsumoApi consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+//		System.out.println(json);
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
-		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
 	}
 }
---
+++ src/main/java/br/com/yan/screenmatch/model/DadosSerie.java
@@ -0,0 +1,12 @@
+package br.com.yan.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonProperty("imdbVotes") String votos) {
+}
