b68ea93bd26945f5b38f48eaa1df2d996622adb4
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-14 06:55:20 -0300

    feat: add Episodio class and integrate it into Principal
    Introduced the Episodio class to represent episode data with attributes such as title, season, episode number, rating, and release date. Updated the Principal class to create and display a list of Episodio objects from API data.

---
+++ src/main/java/br/com/yan/screenmatch/model/Episodio.java
@@ -0,0 +1,80 @@
+package br.com.yan.screenmatch.model;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+public class Episodio {
+    private String titulo;
+    private Integer temporada;
+    private Integer numeroEpisodio;
+    private Double avaliacao;
+    private LocalDate dataLancamento;
+
+    public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
+        this.titulo = dadosEpisodio.titulo();
+        this.temporada = numeroTemporada;
+        this.numeroEpisodio = dadosEpisodio.numero();
+
+        try {
+            this.avaliacao = Double.valueOf(dadosEpisodio.avaliacao());
+        } catch (NumberFormatException ex) {
+            this.avaliacao = 0.0;
+        }
+
+        try {
+            this.dataLancamento = LocalDate.parse(dadosEpisodio.dataLancamento());
+        } catch (DateTimeParseException ex) {
+            this.dataLancamento = null;
+        }
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTemporada() {
+        return temporada;
+    }
+
+    public void setTemporada(Integer temporada) {
+        this.temporada = temporada;
+    }
+
+    public Integer getNumeroEpisodio() {
+        return numeroEpisodio;
+    }
+
+    public void setNumeroEpisodio(Integer numeroEpisodio) {
+        this.numeroEpisodio = numeroEpisodio;
+    }
+
+    public LocalDate getDataLancamento() {
+        return dataLancamento;
+    }
+
+    public void setDataLancamento(LocalDate dataLancamento) {
+        this.dataLancamento = dataLancamento;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Temporada= " + temporada +
+                        ", Título='" + titulo + '\'' +
+                        ", Numero do Episódio=" + numeroEpisodio +
+                        ", Avaliação=" + avaliacao +
+                        ", Data de Lançamento=" + dataLancamento;
+    }
+}
