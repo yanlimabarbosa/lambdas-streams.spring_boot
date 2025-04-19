0507f21923d62c1dc67b1ca2d059af70c130786c
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-19 02:55:44 -0300

    feat: add method to convert JSON to a list in ConverteDados
    Implemented the `obterLista` method in the ConverteDados class to facilitate the conversion of JSON strings into lists of specified types. Updated the IConverteDados interface to include this new method, enhancing data processing capabilities.

--- tabela-fipe-app/src/main/java/com/yan/TabelaFipe/service/IConverteDados.java
+++ tabela-fipe-app/src/main/java/com/yan/TabelaFipe/service/IConverteDados.java
@@ -1,5 +1,9 @@
 package com.yan.TabelaFipe.service;
 
+import java.util.List;
+
 public interface IConverteDados {
     <T> T obterDados(String json, Class<T> classe);
+
+    <T> List<T> obterLista(String json, Class<T> classe);
 }
