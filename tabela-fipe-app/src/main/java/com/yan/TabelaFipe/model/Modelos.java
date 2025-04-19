0e411f8cd8e9ceb2c20e3a3f6c13220023db8fb9
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-19 02:56:07 -0300

    feat: add Modelos record for structured representation of model data
    Introduced the Modelos record to encapsulate a list of Dados, enhancing the data model for the Tabela Fipe application and improving data handling capabilities.

---
+++ tabela-fipe-app/src/main/java/com/yan/TabelaFipe/model/Modelos.java
@@ -0,0 +1,8 @@
+package com.yan.TabelaFipe.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Modelos(List<Dados> modelos) {}
