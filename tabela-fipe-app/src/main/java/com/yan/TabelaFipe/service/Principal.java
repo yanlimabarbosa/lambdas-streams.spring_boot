eb5b0fdc77c378dd9bef78649821e43008cde075
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-19 02:56:14 -0300

    feat: enhance menu options and data retrieval in Principal class
    Updated the Principal class to include a menu for selecting vehicle types (Carro, Moto, Caminhão) and implemented data retrieval from the API based on user input. Added functionality to display vehicle models for the selected brand, improving user interaction and data presentation.

--- tabela-fipe-app/src/main/java/com/yan/TabelaFipe/service/Principal.java
+++ tabela-fipe-app/src/main/java/com/yan/TabelaFipe/service/Principal.java
@@ -1,13 +1,63 @@
 package com.yan.TabelaFipe.service;
 
+import com.yan.TabelaFipe.model.Dados;
+import com.yan.TabelaFipe.model.Modelos;
+
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
 
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1";
 
     public void exibeMenu() {
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consultar:
+                """;
+
+        System.out.println(menu);
+        var opcao = leitura.nextLine();
+
+        String endereco = null;
+
+        if (opcao.toLowerCase().contains("carro")) {
+            endereco = URL_BASE + "/carros/marcas";
+        } else if (opcao.toLowerCase().contains("moto")) {
+            endereco = URL_BASE + "/motos/marcas";
+        } else if (opcao.toLowerCase().contains("caminhão")) {
+            endereco = URL_BASE + "/caminhoes/marcas";
+        } else {
+            System.out.println("Opcão Inválida");
+        }
+
+        var json = consumoApi.obterDados(endereco);
+        System.out.println(json);
+
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+        System.out.println("Informe o código da marca que desja consultar: ");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumoApi.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
+
+
     }
 }
